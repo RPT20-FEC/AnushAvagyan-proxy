cb5da2d59e4d3e718dbfaa7a06e5a112b2ad8c10
Anush Avagyan <57200991+AnushAvagyan@users.noreply.github.com>
2020-05-04 21:11:23 -0700

    Initial setup to render all 4 components - only 3 got rendered for now

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../public'));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../public/index.html'));
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy server is listening on port ${PORT}`);
+});
